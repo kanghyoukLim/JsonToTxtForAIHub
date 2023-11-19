03134d9563bf7e0f21fa76c7cb05c0644f46116c
James Lim <over96@naver.com>
2023-11-19 23:45:23 +0900

    Create movefilesV2_anotherfolder_persentage.py

---
+++ movefilesV2_anotherfolder_persentage.py
@@ -0,0 +1,52 @@
+'''
+원본 파일을 새로운 폴더로 랜덤 비율만큼 이동시키기
+단, 원본파일은 txt와 jpg 가 쌍으로 존재하고 쌍으로 이동된다.
+Yolo Segmentation을 위한 프로그램
+20231119 by KH.Lim
+'''
+
+
+import os
+import random
+import shutil
+
+# 원본 폴더와 목적지 폴더 경로 설정
+source_folder = '020310txt'
+destination_folder = '020310txt_moved'
+
+# 만약 목적지 폴더가 없으면 새로 생성
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+# 원본 폴더 내의 파일 목록 가져오기
+all_files = os.listdir(source_folder)
+
+# 원본 폴더 내의 'txt' 파일 목록 가져오기
+txt_files = [file for file in os.listdir(source_folder) if file.endswith('.txt')]
+
+# 일정 비율로 복사할 'txt' 파일의 수 계산 (예: 전체 파일 중 30%)
+percentage_to_copy = 0.3
+num_files_to_copy = int(len(txt_files) * percentage_to_copy)
+
+# 파일 이름이 동일한 쌍 찾기
+file_pairs = {}
+for file_name in all_files:
+    base_name, extension = os.path.splitext(file_name)
+    if base_name in file_pairs:
+        file_pairs[base_name].append(file_name)
+    else:
+        file_pairs[base_name] = [file_name]
+
+# 동일한 이름을 가진 쌍 중 랜덤하게 선택하여 복사하기
+selected_pairs = [pair for pair in file_pairs.values() if len(pair) >= 2]
+random.shuffle(selected_pairs)
+selected_pairs = selected_pairs[:min(num_files_to_copy, len(selected_pairs))]
+
+for pair in selected_pairs:
+    txt_file = [file for file in pair if file.endswith('.txt')][0]
+    jpg_file = [file for file in pair if file.endswith('.jpg')][0]
+
+    # 파일 복사
+    shutil.copy(os.path.join(source_folder, txt_file), destination_folder)
+    shutil.copy(os.path.join(source_folder, jpg_file), destination_folder)
+
